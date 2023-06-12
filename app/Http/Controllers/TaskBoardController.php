a6557d44dff0b5e64d78fd81c085f2009fbbe2c7
Prem Lamsal <premlamsal2@gmail.com>
2023-06-12 22:05:55 +0545

    update

--- app/Http/Controllers/TaskBoardController.php
+++ app/Http/Controllers/TaskBoardController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 
 class TaskBoardController extends Controller
 {
+    public function __construct()
+    {
+
+        $this->middleware('auth:api');
+
+    }
+
     public function updateTasks(Request $request)
     {
         $this->authorize('hasPermission', 'update_task');
