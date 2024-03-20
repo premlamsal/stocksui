dea4a2452f540f7097d02c93dc755d644dad0dca
Mona Lisa <premlamsal2@gmail.com>
2024-03-20 19:03:23 +0545

    update

--- resources/js/components/app/info.vue
+++ resources/js/components/app/info.vue
@@ -2,15 +2,19 @@
   <div>
     <!-- Page Heading -->
     <h1 class="h3 mb-2 text-gray-800">Application Information</h1>
-    <p class="mb-4">
-    </p>
+    <p class="mb-4"></p>
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
       <!--  <div class="card-header py-3">
               
             </div> -->
       <div class="card-body">
-        Aweseome Invoice software solves the problem of traditional way of Billing and managing stock. This application is developed by BatikLeaf. This application is curretly on beta testing mode. You can always report the bugs and problems to developer or company site.
+        This software is developed by PremLamsal. Contact email:
+        premlamsal2@gmail.com. You can whatsapp me +9779868616747 Aweseome
+        software solves the problem of traditional way of Billing and managing
+        stock. This application is developed by BatikLeaf. This application is
+        curretly on beta testing mode. You can always report the bugs and
+        problems to developer or company site.
         <h6 class="m-0 mt-2 font-weight-bold text-primary">Licence</h6>
         <p></p>
       </div>
@@ -20,24 +24,15 @@
 
 <script>
 export default {
-
   data() {
-    return {
-
-
-    }
+    return {};
   },
   created() {
     //this block will execute when component created
-
-
   },
 
   methods: {
-
-
     //end of methods block
-  }
-
-}
+  },
+};
 </script>
